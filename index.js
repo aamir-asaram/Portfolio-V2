986f87e6feda7f7fbaa6bc5fe13089b7c926d0f5
Aamir <aamirasaram2107@gmail.com>
2023-06-16 17:37:17 +0200

    Add dynamic greeting

---
+++ index.js
@@ -0,0 +1,24 @@
+import { DateTime, Interval } from "./node_modules/luxon/src/luxon.js";
+
+const date = DateTime.local();
+let timeIndex = 2;
+if (Interval.fromDateTimes(
+  DateTime.fromObject({ hour: 12 }),
+  DateTime.fromObject({ hour: 17 })
+).contains(date)) {
+  timeIndex = 1;
+} else if (Interval.fromDateTimes(
+  DateTime.fromObject({ hour: 0 }),
+  DateTime.fromObject({ hour: 12 })
+).contains(date)) {
+  timeIndex = 0;
+}
+console.log(timeIndex);
+const timeOfDay = [
+  'morning',
+  'afternoon',
+  'evening',
+];
+
+const greeting = document.querySelector('#about-title');
+greeting.textContent = `Good ${timeOfDay[timeIndex]}`;
