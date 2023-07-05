a5b211fa2e200497aa614efb9a0db88bb6a627fb
Aamir <aamirasaram2107@gmail.com>
2023-07-05 18:45:58 +0200

    remove luxon

--- index.js
+++ index.js
@@ -1,28 +1,16 @@
-import { DateTime, Interval } from "./node_modules/luxon/src/luxon.js";
-
 const setGreetings = () => {
-  const date = DateTime.local();
-let timeIndex = 2;
-if (Interval.fromDateTimes(
-  DateTime.fromObject({ hour: 12 }),
-  DateTime.fromObject({ hour: 17 })
-).contains(date)) {
-  timeIndex = 1;
-} else if (Interval.fromDateTimes(
-  DateTime.fromObject({ hour: 0 }),
-  DateTime.fromObject({ hour: 12 })
-).contains(date)) {
-  timeIndex = 0;
-}
-const timeOfDay = [
-  'morning',
-  'afternoon',
-  'evening',
-];
+  const date = new Date();
+  const timeIndex = Math.floor(date.getHours() / 8);
 
-const greeting = document.querySelector('#about-title');
-greeting.textContent = `Good ${timeOfDay[timeIndex]}`;
-}
+  const timeOfDay = [
+    'morning',
+    'afternoon',
+    'evening',
+  ];
+
+  const greeting = document.querySelector('#about-title');
+  greeting.textContent = `Good ${timeOfDay[timeIndex]}`;
+  }
 
 setInterval(setGreetings, 1000);
 
