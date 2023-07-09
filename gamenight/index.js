434bb9d4a3c173199118fe4f3c3eff1318e406ee
Aamir <aamirasaram2107@gmail.com>
2023-07-09 17:12:16 +0200

    Add menu

--- gamenight/index.js
+++ gamenight/index.js
@@ -2,10 +2,12 @@ const container = document.querySelector('#content');
 console.log(container);
 const wifi = () => {
   container.innerHTML = `
+  <div id="home-content">
     <h2>WiFi Info</h2>
     <img src="./wifi.png" alt="">
     <p>SSID: <span id="ssid">Home</span></p>
     <p>Password: <span id="password">42884866</span></p>
+  </div>
   `;
   const ssid = document.querySelector('#ssid');
   const password = document.querySelector('#password');
@@ -19,14 +21,21 @@ const wifi = () => {
 };
 
 const home = () => {
+  //write a paragraph about a game night invite
   container.innerHTML = `
-    <p>Lorem ipsum dolor sit amet
-    consectetur adipisicing elit. Quisquam
-    voluptatum, quibusdam, quia, quod
-    voluptates voluptatem quos dolorum
-    voluptate quas quibusdam, quia, quod
-    </p>
+    <div id="home-content">
+      <p id="home">
+        <b>21 07 23</b>
+      </p>
+      <p id="home">
+        <b>Game Night Pt. 2</b>
+      </p>
+      <p id="home">
+        <b>3:00 PM - 9:00 PM</b>
+      </p>
+    </div>
     `;
+
 }
 
 document.addEventListener('click', (e) => {
@@ -36,5 +45,35 @@ document.addEventListener('click', (e) => {
     home();
   }
 });
+
+const hamburger = () => {
+  const menu = document.createElement('div');
+  menu.classList.add('menu');
+  menu.innerHTML = `
+  <i class="fas fa-times" id="close"></i>
+  <p>Home</p>
+  <p>Address</p>
+  <p>WiFi</p>
+  `;
+  container.appendChild(menu);
+  menu.addEventListener('click', (e) => {
+    if (e.target.textContent === 'WiFi') {
+      menu.remove();
+      wifi();
+    } else if (e.target.textContent === 'Home') {
+      home();
+    } else if (e.target.textContent === 'Address') {
+      menu.remove();
+      address();
+    } else if (e.target.id === 'close') {
+      menu.remove();
+    }
+  });
+};
+
+const menu = document.querySelector('#menu');
+menu.addEventListener('click', hamburger);
+
+// hamburger();
 
 home();
