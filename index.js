f14c0d5c3f76c4771a241ba3e0a9687ec5e523de
Aamir <aamirasaram2107@gmail.com>
2023-07-27 14:42:39 +0200

    Add detail cards to project section

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ prev.addEventListener('click', () => {
   title.textContent = titles[index];
 });
 
-const projects = [
+const projectList = [
   {
     title: 'Code & Coffee',
     description: 'A website showcasing an event for developers to meet and share ideas.',
@@ -66,3 +66,82 @@ const projects = [
     tags: ['HTML', 'CSS', 'JavaScript'],
   },
 ]
+
+const projects = document.querySelectorAll('.skill');
+
+//move all projects after the clicked one down 100px
+const moveDown = (clicked) => {
+  projects.forEach((project) => {
+    if (project !== clicked  && project.offsetTop > clicked.offsetTop) {;
+      project.style.transition = 'transform 0.7s ease-in-out';
+    }
+  });
+};
+
+let openProject = false;
+let openedProject = null;
+
+//move all projects back to their original position
+const moveBack = () => {
+  projects.forEach((project) => {
+    project.style.transform = 'translateY(0)';
+    project.style.transition = 'transform 1s ease-in-out';
+  });
+}
+
+//add event listeners to all projects
+projects.forEach((project) => {
+  project.addEventListener('click', () => {
+    if (openProject && openedProject === project) {
+      console.log('same');
+      removeProjectDetails();
+      moveBack();
+      openProject = false;
+      openedProject = null;
+    } else if (openProject) {
+      removeProjectDetails();
+      moveDown(project);
+      addProjectDetails(project);
+      openProject = true;
+      openedProject = project;
+    } else {
+      moveDown(project);
+      addProjectDetails(project);
+      openProject = true;
+      openedProject = project;
+    }
+  });
+});
+
+//add a div with the project details below the clicked project
+const addProjectDetails = (project) => {
+  const projectDetails = document.createElement('div');
+  projectDetails.classList.add('project-details');
+  projectDetails.innerHTML = `
+    <h2>Code & Coffee</h2>
+  `;
+  //add details as next sibling of the clicked project
+  project.parentElement.insertBefore(projectDetails, project.nextSibling);
+
+  projectDetails.style.transform = 'translateX(-100vw)';
+
+  //add a transition to the project details
+  setTimeout(() => {
+    projectDetails.style.transform = 'translateX(0)';
+    projectDetails.style.transition = 'transform 0.7s ease-in-out';
+  }, 100);
+}
+
+//remove all project details
+const removeProjectDetails = () => {
+  const details = document.querySelectorAll('.project-details');
+  console.log(details);
+  details.forEach((detail) => {
+    //add a transition to the project details
+    detail.style.transform = 'translateX(-100vw)';
+    detail.style.transition = 'transform 0.7s ease-in-out';
+    setTimeout(() => {
+      detail.parentElement.removeChild(detail);
+    }, 700);
+  })
+}
